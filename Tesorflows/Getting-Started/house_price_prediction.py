df984bc4920932772aef7fc316a2913c9396c1e5
dungkarl <dungdinh731989@gmail.com>
2021-02-19 18:04:20 +0700

    add house prdeiction

---
+++ Tesorflows/Getting-Started/house_price_prediction.py
@@ -0,0 +1,13 @@
+"""
+	Prediction price using tensorflow
+"""
+
+import tensorflow as tf 
+import numpy as np 
+import pandas as pd 
+import math
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+
